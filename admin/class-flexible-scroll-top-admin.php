bb37a53c683d7e59ccb6867410bde6faa4deaf59
Bowo <wibowosulistio@gmail.com>
2022-10-26 08:21:37 +0700

    Improve admin page header and footer links
    Improve admin page header and footer links

--- admin/class-flexible-scroll-top-admin.php
+++ admin/class-flexible-scroll-top-admin.php
@@ -122,7 +122,7 @@ class Flexible_Scroll_Top_Admin {
 				'menu_parent'		=> 'options-general.php',
 				'menu_position'		=> 100,
 				// 'menu_icon'			=> 'dashicons-arrow-up-alt2',
-				'framework_title' 	=> 'Flexible Scroll Top <small>by <a href="https://bowo.io" target="_blank">bowo.io</a> | <a href="https://wordpress.org/plugins/flexible-scroll-top/#reviews" target="_blank" class="header-action">Review</a> &bull; <a href="https://wordpress.org/support/plugin/flexible-scroll-top/" target="_blank" class="header-action">Feedback</a> &bull; <a href="https://paypal.me/qriouslad" target="_blank" class="header-action">Donate</a></small>',
+				'framework_title' 	=> 'Flexible Scroll Top <small>by <a href="https://bowo.io" target="_blank">bowo.io</a> <a href="https://wordpress.org/plugins/flexible-scroll-top/" target="_blank" class="fst-header-action"><span>&#8505;</span> Info</a><a href="https://wordpress.org/plugins/flexible-scroll-top/#reviews" target="_blank" class="fst-header-action"><span>&starf;</span> Review</a><a href="https://wordpress.org/support/plugin/flexible-scroll-top/" target="_blank" class="fst-header-action">&#10010; Feedback</a><a href="https://paypal.me/qriouslad" target="_blank" class="fst-header-action">&#9829; Donate</a></small>',
 				'framework_class' 	=> 'fst-options',
 				'show_bar_menu' 	=> false,
 				'show_search' 		=> false,
@@ -131,7 +131,7 @@ class Flexible_Scroll_Top_Admin {
 				'show_form_warning' => false,
 				'save_defaults'		=> true,
 				'show_footer' 		=> false,
-				'footer_credit'		=> '<a href="https://wordpress.org/plugins/flexible-scroll-top/" target="_blank">Flexible Scroll Top</a> (<a href="https://github.com/qriouslad/flexible-scroll-top" target="_blank">github</a>) is built with the <a href="https://github.com/devinvinson/WordPress-Plugin-Boilerplate/" target="_blank">WordPress Plugin Boilerplate</a>, <a href="https://wppb.me" target="_blank">wppb.me</a>, <a href="https://github.com/Codestar/codestar-framework" target="_blank">CodeStar</a> framework, <a href="https://github.com/CodyHouse/back-to-top" target="_blank">Back to Top</a> library and <a href="https://freeicons.io/" target="_blank">freeicons.io</a> icons.',
+				'footer_credit'		=> '<a href="https://wordpress.org/plugins/flexible-scroll-top/" target="_blank">Flexible Scroll Top</a> (<a href="https://github.com/qriouslad/flexible-scroll-top" target="_blank">github</a>) is built with the <a href="https://github.com/devinvinson/WordPress-Plugin-Boilerplate/" target="_blank">WordPress Plugin Boilerplate</a>, <a href="https://wppb.me" target="_blank">wppb.me</a>, <a href="https://github.com/Codestar/codestar-framework" target="_blank">CodeStar</a>, <a href="https://github.com/CodyHouse/back-to-top" target="_blank">Back to Top</a> and <a href="https://freeicons.io/" target="_blank">freeicons.io</a> icons.',
 			) );
 
 			// Create Button Options section
